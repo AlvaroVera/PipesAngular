e9a23c25f1a602f5baf777f4db74062764b94230
Alvaro Vera <alvaro291095@gmail.com>
2019-04-30 19:37:24 -0500

    Pipe Json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pipes';
+  nombre = 'Alvaro';
+  arreglo = [1,2,3,4,5,6,7,8,9,10];
+  PI = Math.PI;
+  a: number = 0.234;
+  pago: number = 1.3495;
+  jsonPrueba: Object = {
+    foo: 'bar', 
+    baz: 'qux', 
+    nested: {
+      xyz: 3, 
+      numbers: [1, 2, 3, 4, 5]
+    }
+  };
 }
